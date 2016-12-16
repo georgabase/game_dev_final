c6faea3b4b285f2e5da95dc84108533792aea97f
georgabase <swabston@gmail.com>
2016-12-16 17:19:33 -0500

    rock to the rythm with the

---
+++ Assets/Scripts/Music2.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Music2 : MonoBehaviour {
+	private GameObject menuMusic;
+
+
+	// Use this for initialization
+	void Start () {
+		// see if we've got menu music still playing
+		menuMusic = GameObject.Find("MenuMusic");
+		if (menuMusic) {
+			// kill menu music
+			Destroy(menuMusic);
+		}
+		// make sure we survive going to different scenes
+		DontDestroyOnLoad(gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+
+	public AudioClip SoundClip;
+	private AudioSource SoundSource;
+	private GameObject gameMusic;
+
+	void Awake()
+	{
+		// see if we've got game music still playing
+		 gameMusic = GameObject.Find("GameMusic");
+		if (gameMusic) {
+			// kill game music
+			Destroy(gameMusic);
+		}
+		// make sure we survive going to different scenes
+		DontDestroyOnLoad(gameObject);
+}
+}
